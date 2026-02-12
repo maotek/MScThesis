e5a329bd887794fafd6ac1580cc21c06a2669251
maotek <maoshengj@hotmail.com>
2026-02-12 11:06:26 +0100

    fix job file

--- code/concentrate_dav2.sh
+++ code/concentrate_dav2.sh
@@ -8,13 +8,13 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=12:00:00
+#SBATCH --time=36:00:00
 #SBATCH --gres=gpu:a40:1
-#SBATCH --mem=4096
+#SBATCH --mem=160000
 
 module use /opt/insy/modulefiles # Use DAIC INSY software collection
 module load cuda cudnn miniconda # Load certain versions of cuda and cudnn
 
 cd MScThesis/code
 
-apptainer exec --nv ../../apptainer/image.sif python -m train_concentration_dav2 --config configs/concentration_dav2.yaml
+apptainer exec --nv ../../apptainer/image.sif python -m train_concentration_dav2 --config configs/train/concentration_dav2_voxelgrid.json
