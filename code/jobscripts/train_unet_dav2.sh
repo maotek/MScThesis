605e31072bdafe302cc6f79a8f07e2883d3b2209
maotek <maoshengj@hotmail.com>
2026-03-05 01:01:43 +0100

    add new configs

--- code/jobscripts/train_unet_dav2.sh
+++ code/jobscripts/train_unet_dav2.sh
@@ -17,4 +17,4 @@ module load cuda cudnn miniconda # Load certain versions of cuda and cudnn
 
 cd MScThesis/code
 
-apptainer exec --nv ../../apptainer/image.sif python -m train_unet_dav2 --config configs/train/unet_dav2.json --save-dir output/train_unet_dav2
+apptainer exec --nv ../../apptainer/image.sif python -m train_unet_dav2 --config configs/dsec/train/unet_dav2.json --save-dir output/train_unet_dav2
