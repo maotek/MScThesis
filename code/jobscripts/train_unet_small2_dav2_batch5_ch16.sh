17c05ccc8da63062964145c458a431a798646eba
maotek <maoshengj@hotmail.com>
2026-03-09 06:17:35 +0100

    add jobs

---
+++ code/jobscripts/train_unet_small2_dav2_batch5_ch16.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=train_unet_small2_dav2_batch5_ch16
+#SBATCH --output=train_unet_small2_dav2_batch5_ch16_%j.out
+#SBATCH --error=train_unet_small2_dav2_batch5_ch16_%j.err
+#SBATCH --account=ewi-insy-prb
+#SBATCH --partition=insy,general
+#SBATCH --qos=medium
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=36:00:00
+#SBATCH --gres=gpu:a40:1
+#SBATCH --mem=160000
+
+module use /opt/insy/modulefiles # Use DAIC INSY software collection
+module load cuda cudnn miniconda # Load certain versions of cuda and cudnn
+
+cd MScThesis/code
+
+apptainer exec --nv ../../apptainer/image.sif python -m train_unet_dav2 --config configs/dsec/train/unet_small2_dav2_batch5_ch16.json --save-dir output/train_unet_small2_dav2_batch5_ch16
