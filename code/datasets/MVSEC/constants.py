ef37150d993491290ab7783d2ed14c8aa714def4
maotek <maoshengj@hotmail.com>
2026-03-18 14:23:41 +0100

    change mvsec constants

--- code/datasets/MVSEC/constants.py
+++ code/datasets/MVSEC/constants.py
@@ -2,11 +2,11 @@ MVSEC_HEIGHT = 260
 MVSEC_WIDTH = 346
 
 MVSEC_TRAIN = {
-    "train/outdoor_day2":12178,
+    "train/outdoor_day2":12197,
 }
 
 MVSEC_TEST = {
-    "test/outdoor_day1":5125, "test/outdoor_night1":5111, "test/outdoor_night2":5478, "test/outdoor_night3":5411,
+    "test/outdoor_day1":5134, "test/outdoor_night1":5133, "test/outdoor_night2":5497, "test/outdoor_night3":5429,
 }
 
 MVSEC_ALL_DATA_FOLDERS = list(MVSEC_TRAIN.keys()) + list(MVSEC_TEST.keys())
