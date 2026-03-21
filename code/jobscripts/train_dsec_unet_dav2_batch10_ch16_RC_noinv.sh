c0ec2e640b77d966459a70c43b1a5dd76cd624a2
maotek <maoshengj@hotmail.com>
2026-03-22 00:50:46 +0100

    update configs for mvsec

--- code/jobscripts/train_dsec_unet_dav2_batch10_ch16_RC_noinv.sh
+++ code/jobscripts/train_dsec_unet_dav2_batch10_ch16_RC_noinv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=train_mvsec_unet_dav2_batch10
-#SBATCH --output=train_mvsec_unet_dav2_batch10_%j.out
-#SBATCH --error=train_mvsec_unet_dav2_batch10_%j.err
+#SBATCH --job-name=train_dsec_unet_dav2_batch10_ch16_RC_noinv
+#SBATCH --output=train_dsec_unet_dav2_batch10_ch16_RC_noinv_%j.out
+#SBATCH --error=train_dsec_unet_dav2_batch10_ch16_RC_noinv_%j.err
 #SBATCH --account=ewi-insy-prb
 #SBATCH --partition=insy,general
 #SBATCH --qos=medium
@@ -17,4 +17,4 @@ module load cuda cudnn miniconda # Load certain versions of cuda and cudnn
 
 cd MScThesis/code
 
-apptainer exec --nv ../../apptainer/image.sif python -m train_unet_dav2 --config-path configs/mvsec/train/unet_dav2_batch10.json
+apptainer exec --nv ../../apptainer/image.sif python -m train_unet_dav2 --config configs/dsec/train/unet_dav2_batch10_ch16_RC_noinv.json
