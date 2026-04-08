7bf437ea22d4ade43a21da3cd0c5ead38fd6bf73
maotek <maoshengj@hotmail.com>
2026-04-08 16:02:29 +0200

    add nonorm for rgb

---
+++ code/jobscripts/train_dsec_unet_dav2_rgb_nonorm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=train_dsec_unet_dav2_rgb_nonorm
+#SBATCH --output=train_dsec_unet_dav2_rgb_nonorm_%j.out
+#SBATCH --error=train_dsec_unet_dav2_rgb_nonorm_%j.err
+#SBATCH --account=ewi-insy-prb
+#SBATCH --partition=insy,general
+#SBATCH --qos=medium
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=36:00:00
+#SBATCH --gres=gpu:a40:1
+#SBATCH --mem=160000
+
+module use /opt/insy/modulefiles # Use DAIC INSY software collection
+module load cuda cudnn miniconda # Load certain versions of cuda and cudnn
+
+cd MScThesis/code
+
+apptainer exec --nv ../../apptainer/image.sif python -m train_unet_dav2 --config configs/dsec/train/unet_dav2_rgb_nonorm.json
