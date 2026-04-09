e10ddb74af70e56fa56913b3141ef1516d2e1e9a
maotek <maoshengj@hotmail.com>
2026-04-09 16:14:21 +0200

    add job for rgb stats

---
+++ code/jobscripts/compute_rgb_stats_dsec.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=rgb_stats_dsec
+#SBATCH --output=rgb_stats_dsec_%j.out
+#SBATCH --error=rgb_stats_dsec_%j.err
+#SBATCH --account=ewi-insy-prb
+#SBATCH --partition=insy,general
+#SBATCH --qos=short
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=1:00:00
+#SBATCH --gres=gpu:a40:1
+#SBATCH --mem=32000
+
+module use /opt/insy/modulefiles # Use DAIC INSY software collection
+module load cuda cudnn miniconda # Load certain versions of cuda and cudnn
+
+cd MScThesis/code
+
+apptainer exec --nv ../../apptainer/image.sif python -m test_dsec.compute_rgb_stats
