6dfa3b56f22f3f07420001b507a25b45f0416563
maotek <maoshengj@hotmail.com>
2026-04-11 02:33:57 +0200

    add significance test

---
+++ code/jobscripts/train_dsec_unet_dav2_batch10_seed3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=train_dsec_unet_dav2_batch10_seed3
+#SBATCH --output=train_dsec_unet_dav2_batch10_seed3_%j.out
+#SBATCH --error=train_dsec_unet_dav2_batch10_seed3_%j.err
+#SBATCH --account=ewi-insy-prb
+#SBATCH --partition=insy,general
+#SBATCH --qos=medium
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=36:00:00
+#SBATCH --gres=gpu:a40:1
+#SBATCH --mem=160000
+
+module use /opt/insy/modulefiles # Use DAIC INSY software collection
+module load cuda cudnn miniconda # Load certain versions of cuda and cudnn
+
+cd MScThesis/code
+
+apptainer exec --nv ../../apptainer/image.sif python -m train_unet_dav2 --seed 3 --config configs/dsec/train/unet_dav2_batch10_seed3.json
