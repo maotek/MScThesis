db9b66e3f8e620d2ac1c30bf565f7de90818d4a3
maotek <maoshengj@hotmail.com>
2026-04-12 01:23:05 +0200

    add cuda support for rgb stats

--- code/jobscripts/compute_rgb_stats_dsec.sh
+++ code/jobscripts/compute_rgb_stats_dsec.sh
@@ -8,12 +8,14 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
+#SBATCH --gpus=1
 #SBATCH --time=4:00:00
 #SBATCH --mem=32000
 
 module use /opt/insy/modulefiles # Use DAIC INSY software collection
 module load miniconda # Load conda
+module load cuda
 
 cd MScThesis/code
 
-apptainer exec ../../apptainer/image.sif python -m test_dsec.compute_rgb_stats
+apptainer exec --nv ../../apptainer/image.sif python -m test_dsec.compute_rgb_stats
