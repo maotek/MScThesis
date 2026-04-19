075cd824d093927166ea392019115a1943e051d5
maotek <maoshengj@hotmail.com>
2026-04-20 01:14:06 +0200

    remove mps

--- code/evaluate.py
+++ code/evaluate.py
@@ -112,7 +112,7 @@ def setup_device_and_seeds(args: argparse.Namespace) -> torch.device:
     np.random.seed(args.seed)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    device = torch.device("mps")
+    # device = torch.device("mps")
     if device.type == "cuda":
         torch.cuda.manual_seed(args.seed)
     return device
