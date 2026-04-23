4c0f88eb2e32b6f1b3710e3ff7d34d8687e17209
maotek <maoshengj@hotmail.com>
2026-04-23 15:05:14 +0200

    add new configs

---
+++ code/jobscripts/train_mvsec_new_fully_conv_dav2_batch10_augmented_seed5.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=train_mvsec_new_fully_conv_dav2_batch10_augmented_seed5
+#SBATCH --output=train_mvsec_new_fully_conv_dav2_batch10_augmented_seed5_%j.out
+#SBATCH --error=train_mvsec_new_fully_conv_dav2_batch10_augmented_seed5_%j.err
+#SBATCH --account=ewi-insy-prb
+#SBATCH --partition=insy,general
+#SBATCH --qos=medium
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=36:00:00
+#SBATCH --gres=gpu:a40:1
+#SBATCH --mem=160000
+
+module use /opt/insy/modulefiles # Use DAIC INSY software collection
+module load cuda cudnn miniconda # Load certain versions of cuda and cudnn
+
+cd MScThesis/code
+
+apptainer exec --nv ../../apptainer/image.sif python -m train_unet_dav2 --seed 5 --config-path configs/mvsec/train/new_fully_conv_dav2_batch10_augmented_seed5.json
